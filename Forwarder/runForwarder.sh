f2cbd7b46321cc55f6991c83e8121cbb8ccb2621
Robert Statsinger <rstatsinger@azul.com>
2024-08-27 16:20:26 +0000

    Upload New File

---
+++ Forwarder/runForwarder.sh
@@ -0,0 +1,13 @@
+# example startup script for the Forwarder
+
+unset AGENTDIR
+unset CRS_TAGS
+unset JDK_JAVA_OPTIONS
+unset _JAVA_OPTIONS
+unset AZ_CRS_ARGUMENTS
+
+wget `curl -s https://cdn.azul.com/forwarder/forwarder.json | grep url | cut -d"\"" -f4`
+
+source ../.env
+
+java -jar ./forwarder.jar
