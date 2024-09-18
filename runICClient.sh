3dd6562557efc00ec2c676f7c7c2938344b61047
Robert Statsinger <rstatsinger@azul.com>
2024-09-18 20:24:58 +0000

    Update runICClient.sh

--- runICClient.sh
+++ runICClient.sh
@@ -5,6 +5,6 @@ unset JDK_JAVA_OPTIONS
 unset _JAVA_OPTIONS
 unset AZ_CRS_ARGUMENTS
 
-source ./.env
+. ./.env
 
 java -jar ./icclient.jar unused-code-diff --appenv=$APPENV --ic=$IC_API_URL --key=$APIKEY --output=./$APPENV-CI_Report
